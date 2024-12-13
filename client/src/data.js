2a41c4c3b7e975b8900654c43f7b92e50945cfc7
bilalmisla <bilal@madeinsouth.la>
2024-12-13 23:22:28 +0700

    web-development

--- client/src/data.js
+++ client/src/data.js
@@ -18,7 +18,7 @@ export const cards = [
     title: "Web Development",
     desc: "Add talent to AI",
     img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/f27bec553efc12cc60baed89b8f2223e-1674661140708/ai-artists-2x.png",
-    slug: 'ai',
+    slug: 'web-development',
   },
   {
     id: 2,
@@ -32,14 +32,14 @@ export const cards = [
     title: "Video Editing",
     desc: "Customize your site",
     img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png",
-    slug: 'wordpress'
+    slug: 'video-editing'
   },
   {
     id: 4,
     title: "Admin Work",
     desc: "Share your message",
     img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png",
-    slug: 'voice'
+    slug: 'admin-work'
   },
   {
     id: 7,
