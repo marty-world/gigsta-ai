48016edc7d50044ac4e1b16ca9f33e283c136139
bilalmisla <bilal@madeinsouth.la>
2024-12-17 20:42:45 +0700

    update home styles

--- client/src/components/Home/LandingSection/index.jsx
+++ client/src/components/Home/LandingSection/index.jsx
@@ -2,44 +2,44 @@ import React from "react";
 import "./landingsection.scss";
 
 const LandingSection = () => {
+    const titleClass = `mb-2 text-white text-[34px] font-semibold leading-[40px] md:text-[38px] md:leading-[44px] xl:text-[48px] xl:leading-[56px]`;
+
     return (
         <div className="theme-section !pt-0">
             <div className="custom-container">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 rounded-[25px] border border-[#4b515a] flex items-center flex-wrap 
                     gap-[20px] sm:gap-[30px] md:gap-[40px] lg:gap-[50px] xl:gap-[60px] section-padding justify-between self-center">
                     <div className="section-top__content section-top-content order-2 md:order-1">
-                        <p className="text-secondary text-xs sm:text-sm md:text-md mb-2 font-medium">
-                            Explore your business
-                        </p>
-                        <h1 className="mb-2 text-white text-[28px] font-semibold leading-[32px] md:text-[36px] md:leading-[40px] lg:text-[48px] lg:leading-[52px] xl:text-[64px] xl:leading-[68px]">
+
+                        <h1 className={titleClass}>
                             A whole world of freelance talent at your fingertips
                         </h1>
-                        <ul className="section-top-content__list card-list">
-                            <li className="card-list__item">
-                                <p className="card-list__text">
+                        <ul className="section-top-content__list mt-[25px] font-[16px] ">
+                            <li className="card-list__item pb-[10px]">
+                                <p className="card-list__text  lg:text-[18px]">
                                     The best for every budget
                                 </p>
                             </li>
-                            <li className="card-list__item">
-                                <p className="card-list__text">
+                            <li className="card-list__item pb-[10px]">
+                                <p className="card-list__text  lg:text-[18px]">
                                     Quality work done quickly
                                 </p>
                             </li>
-                            <li className="card-list__item">
-                                <p className="card-list__text">
+                            <li className="card-list__item pb-[10px]">
+                                <p className="card-list__text lg:text-[18px]">
                                     Protected payments, every time
                                 </p>
                             </li>
-                            <li className="card-list__item">
-                                <p className="card-list__text">
+                            <li className="card-list__item pb-[10px]">
+                                <p className="card-list__text lg:text-[18px]">
                                     24/7 support
                                 </p>
                             </li>
                         </ul>
-                        <div className="section-top-content__buttons">
+                        <div className="section-top-content__buttons mt-[25px]">
                             <a className="section-top-content__buttons-link section-top-content__buttons-link--green"
                                 href="#">
-                                Explore your business
+                                Explore Our Gigs
                             </a>
                         </div>
                     </div>
